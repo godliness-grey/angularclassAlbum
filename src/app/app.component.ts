84f65cb1b995314590e042124355db47648c4956
Kizito Okechukwu <anyanwuokechukwuk@gmail.com>
2020-01-28 08:58:24 +0400

    my updated commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   name = `Tosin-Omotayo`;
   show = `ball-show`;
   academy = [
-    { name: "tosin", image: "sf.jpg",complexion: "black", hobbies: ["dance", "talk"] },
-    { name: "lola", image: "sf.jpg",complexion: "yellow", hobbies: ["swim", "wyne"] },
-    { name: "wale", image: "sf.jpg",complexion: "fair", hobbies: ["code", "code"] },
-    { name: "charlse", image: "sf.jpg",complexion: "black", hobbies: ["drive", "talk"] }
+    { name: "tosin", image: "kizi1.jpg",complexion: "black", hobbies: ["dance", "talk"] },
+    { name: "lola", image: "kizi2.jpg",complexion: "yellow", hobbies: ["swim", "wyne"] },
+    { name: "wale", image: "kizi3.jpg",complexion: "fair", hobbies: ["code", "code"] },
+    { name: "charlse", image: "kizi4.jpg",complexion: "black", hobbies: ["drive", "talk"] }
   ];
 }
